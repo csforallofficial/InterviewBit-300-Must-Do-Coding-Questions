2e2d0f17c9e80c15c3ae8ce54cfc863c7207042e
Sunyul Hossen <60459287+xenonreborn@users.noreply.github.com>
2021-10-25 18:10:24 +0530

    Create Majority Element.cpp

---
+++ Majority Element.cpp
@@ -0,0 +1,38 @@
+/*
+Majority Element
+Easy
+
+86
+
+6
+
+Add to favorites
+Asked In:
+MICROSOFT
+YAHOO
+GOOGLE
+AMAZON
+Problem Description
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example :
+
+Input : [2, 1, 2]
+Return  : 2 which occurs 2 times which is greater than 3/2.
+*/
+
+
+int Solution::majorityElement(const vector<int> &A) {
+    int candidate = 0, count = 0;
+    for(auto num:A){
+        if(count == 0)
+            candidate = num;
+        if(candidate == num)
+            count++;
+        else
+            count--;
+    }
+    return candidate;
+}
